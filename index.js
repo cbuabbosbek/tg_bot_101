d42f59f92a40d2560faaccf54f7d2433abd9daf6
abbossth <a@test.ru>
2025-11-04 17:06:07 +0500

    done

--- index.js
+++ index.js
@@ -4,14 +4,38 @@ const TOKEN = "8415282558:AAFhRN-qCJ7Bz3enueyC3S2LNpOGKAnQM-M";
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-bot.on("message", function (msg) {
+bot.on("message", async function (msg) {
+  console.log(msg);
   const chatId = msg.chat.id;
   const text = msg.text;
+  const firstName = msg.chat.first_name;
+
+  if (text == "/start") {
+    bot.sendMessage(chatId, `Xush kelibsiz, ${firstName} 👋`, {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Boshlash 🔥" }],
+          [{ text: "Menu 🍔" }, { text: "Sozlamalar ⚙️" }],
+        ],
+        resize_keyboard: true,
+      },
+    });
+  } else if (text == "Boshlash 🔥") {
+    const xabar = await bot.sendMessage(chatId, "Iltimos, kuting...");
+
+    setTimeout(function () {
+      bot.deleteMessage(chatId, xabar.message_id);
+      // bot.sendMessage(chatId, "1 sekund kechikdi");
+      bot.sendPhoto(chatId, "./images/urus.jpeg", {
+        caption: ` The Urus has a V8 twin-turbocharged engine with a maximum power output of 650 hp, and offers the best power-to-weight ratio in the SUV segment. `,
+      });
+    }, 1000);
+  } else if (text == "Menu 🍔") {
+    bot.sendMessage(chatId, "Menyuga xush kelibsiz");
+  }
 
   console.log("Start");
-  bot.sendMessage(chatId, `Salom --> ${text}`);
   console.log("Xabar keldi");
-  console.log(msg);
 });
 
 console.log("Botimiz ishga tushdi");
